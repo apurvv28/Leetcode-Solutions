f9dd6d128130b8d5b15af41962f589451997b863
Apurv Saktepar <108170788+apurvv28@users.noreply.github.com>
2026-07-28 23:46:50 +0530

    Time: 0 ms (100%), Space: 11.6 MB (95.98%) - LeetHub

---
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head, Node* rest = nullptr) {
+     Node* curr = head;
+     while(curr){
+        if(curr->child!=nullptr){
+            Node* nextNode = curr->next;
+            Node* childNode = curr->child;
+            curr->next = childNode;
+            childNode->prev = curr;
+            curr->child = nullptr;
+
+            Node* temp = childNode;
+            while(temp->next){
+                temp = temp->next;
+            }
+            if(nextNode){
+                temp->next = nextNode;
+            }
+            if(nextNode){
+                nextNode->prev = temp;
+            }
+        }
+        curr = curr->next;
+     }   
+     return head;
+    }
+};
