3d76258d0c1707d3c688160e258e11ab6ecf4c26
Apurv Saktepar <108170788+apurvv28@users.noreply.github.com>
2026-08-01 00:10:25 +0530

    Time: 0 ms (100%), Space: 118 MB (82.29%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,11 +10,11 @@
  */
 class Solution {
 public:
-    ListNode * reversel(ListNode *head)
+    ListNode * reversal(ListNode *head)
     {
-        ListNode* prev = nullptr;
         ListNode* curr = head;
-        while(curr!=nullptr){
+        ListNode* prev = nullptr;
+        while(curr){
             ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
@@ -23,22 +23,19 @@ public:
         return prev;
     }
     bool isPalindrome(ListNode* head) {
-        ListNode *dummy=new ListNode(0,head);
+        ListNode* dummy = new ListNode(0, head);
         ListNode* slow = dummy;
         ListNode* fast = dummy;
-
-        //Find Mid by using fast & slow pointers
-        while(fast!=nullptr&&fast->next!=nullptr){
+        while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
-        ListNode *second=slow->next;
-        slow->next=nullptr;
-        //Reverse function
-        //Compare two lists
+        ListNode* second = slow->next;
+        slow->next = nullptr;
+
         ListNode* temp1 = head;
-        ListNode* temp2 = reversel(second);
-        while(temp2!=nullptr){
+        ListNode* temp2 = reversal(second);
+        while(temp2){
             if(temp1->val != temp2->val){
                 return false;
             }
