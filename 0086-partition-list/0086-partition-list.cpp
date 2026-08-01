4045a94076da6b3437efbc814ef4a0e038a092e9
Apurv Saktepar <108170788+apurvv28@users.noreply.github.com>
2026-08-01 23:44:48 +0530

    Time: 0 ms (100%), Space: 15 MB (11.6%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,10 +11,10 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode dummy1(0);
-        ListNode dummy2(0);
-        ListNode* l1 = &dummy1;
-        ListNode* l2 = &dummy2;
+        ListNode* dummy1 = new ListNode(0, head);
+        ListNode* dummy2 = new ListNode(0, head);
+        ListNode* l1 = dummy1;
+        ListNode* l2 = dummy2;
         while(head){
             if(head->val<x){
                 l1 = l1->next = head;
@@ -23,8 +23,8 @@ public:
             }
             head = head->next;
         }
-        l2->next = NULL;
-        l1->next = dummy2.next;
-        return dummy1.next;
+        l2->next = nullptr;
+        l1->next = dummy2->next;
+        return dummy1->next;
     }
 };
