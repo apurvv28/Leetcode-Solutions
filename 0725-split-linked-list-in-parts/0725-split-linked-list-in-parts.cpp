83a1703295fef72712b713485e1a50e4e2d6735f
Apurv Saktepar <108170788+apurvv28@users.noreply.github.com>
2026-08-01 23:55:50 +0530

    Time: 0 ms (100%), Space: 13.6 MB (85.68%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.cpp
@@ -10,40 +10,27 @@
  */
 class Solution {
 public:
-    vector<ListNode*> splitListToParts(ListNode* root, int k) {
-        //Create a vector of linked list pointers
+    vector<ListNode*> splitListToParts(ListNode* head, int k) {
         vector<ListNode*> parts(k, nullptr);
-
-        //Find length of linked list
         int len = 0;
-        for (ListNode* node = root; node; node = node->next)
+        ListNode* temp = head;
+        while(temp){
+            temp=temp->next;
             len++;
-
-        // Calculate minimum guaranteed part size (n) and number of extra nodes (r)
-        int n = len / k;
-        int r = len % k;
-
-        // Initialize the pointers to traverse LL
-        ListNode* node = root, *prev = nullptr;
-
-        //Outer Loop
-        for(int i = 0; node && i<k; i++, r--){
-            
-            //Store current node as start of current part
-            parts[i] = node;
-
-            //Traverse n+1 nodes if there are remianing extra nodes (r > 0)
-            //Else traverse n nodes only
-            for(int j = 0; j < n + (r > 0); j++){
-                prev = node;
-                node = node->next;
+        }
+        int n = len/k;
+        int r = len%k;
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        for(int i = 0; i < k && curr; i++, r--){
+            parts[i] = curr;
+            for(int j = 0; j < n+(r>0); j++){
+                prev = curr;
+                curr = curr->next;
             }
-
-            //Disconnect current part from rest of LL
             prev->next = nullptr;
         }
-
-        //Return array of k parts
         return parts;
+        
     }
 };
