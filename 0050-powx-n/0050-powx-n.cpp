7e88800844d1571d1a7f56e24a5b41dcf9bbd141
Apurv Saktepar <108170788+apurvv28@users.noreply.github.com>
2026-09-09 23:24:52 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.81%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     double myPow(double x, int n){
-        if(n==0)
-        {
+        if(n==0){
             return 1;
         }
-        if(n==1)
-        {
+        if(n==1){
             return x;
         }
         long long N=n;
